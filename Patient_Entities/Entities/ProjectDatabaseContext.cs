80c61a0cc701b8d2ee6e09eff510954d1f2e3e68
Bhargav7342 <BhargavBhat7342@gmail.com>
2023-04-12 19:52:00 +0530

    Update

--- Patient_Entities/Entities/ProjectDatabaseContext.cs
+++ Patient_Entities/Entities/ProjectDatabaseContext.cs
@@ -17,7 +17,7 @@ public partial class ProjectDatabaseContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-       => optionsBuilder.UseSqlServer("Server=tcp:bhargav-db-1.database.windows.net,1433;Initial Catalog=ProjectDatabase;Persist Security Info=False;User ID=bhargav7342;Password=Bhargav@420;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+       => optionsBuilder.UseSqlServer("Server=tcp:cibi-server.database.windows.net,1433;Initial Catalog=ProjectDatabase;Persist Security Info=False;User ID=cibi619;Password=Cibi@619;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
 
 
